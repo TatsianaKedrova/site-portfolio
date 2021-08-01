eca034c8e1e87846d9234994cd06e2d1dc948aa9
TatsianaKedrova <Baletrot@gmail.com>
2021-08-01 14:44:24 +0200

    completed desktop version of Projects.tsx

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./Projects.module.css";
 import styleContainer from "./../common/styles/Container.module.css";
+import Project from "./project/Project";
 
 
 function Projects() {
@@ -8,7 +9,10 @@ function Projects() {
         <div className={classes.projectsBlock}>
             <div className={`${styleContainer.container} ${classes.projectsContainer}`}>
                 <h2 className={classes.title}>My Projects</h2>
-
+                <div className={classes.projects}>
+                    <Project name={"Social Network"} description={"This is a social net project that was developed using React-Redux-Typescript technologies. It is full of modern features and is ready to use right now."}/>
+                    <Project name={"TodoList"} description={"This is a Todolist application that is more complicated that it can seem to be. You can perform various actions like reordering lists, making api calls to the server, login and logout and many more fancy actions. React, Redux, Typescript, axios and thunks are just for developing it."}/>
+                </div>
             </div>
         </div>
     )
---
+++ src/projects/project/Project.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import classes from "./Project.module.css";
+
+export type ProjectType = {
+    name: string
+    description: string
+}
+
+function Project(props: ProjectType) {
+    return (
+        <div className={classes.project}>
+            <div className={classes.picture}>
+                <p>This a project's picture</p>
+                <button className={classes.btn}>Open</button>
+            </div>
+            <div>
+                <div>
+                    <p>{props.name}</p>
+                </div>
+                <div>
+                    <span className={classes.description}>{props.description}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Project;
